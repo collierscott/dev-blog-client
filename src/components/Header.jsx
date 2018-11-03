2bf8f73698dcacac63ac0d84faae2c80d5802d18
Scott Collier <scott@onlinespaces.com>
2018-11-03 12:40:33 +0100

    Spinner should show while user profile is loading

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,14 @@ import {NavLink} from 'react-router-dom';
 import Spinner from './Spinner';
 
 const Header = ({isAuthenticated, userData}) => {
-
-  if(isAuthenticated && null === userData) {
-    return (<Spinner />)
-  }
-
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <NavLink exact to="/" className="navbar-brand">Home</NavLink>
         <span className="navbar-text">
             {isAuthenticated ?
+              null === userData ?
+                <i className="fas fa-spinner fa-spin"/>
+                :
               <span>Hello {userData.name}</span> :
               <NavLink exact to="/login">Sign in</NavLink>
             }
